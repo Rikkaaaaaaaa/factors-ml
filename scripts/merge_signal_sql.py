e94eed9d2107e98a3f48ac3cd5e8f7d89ee6b50c
ruanjx <Junxiang.Ruan@cicc.com.cn>
2023-10-25 11:08:19 +0800

    format code

--- scripts/merge_signal_sql.py
+++ scripts/merge_signal_sql.py
@@ -52,6 +52,7 @@ def save2sql(data_name, merge_signal):
 def merge_signal_ensemble(args):
     # zz800 高价股
     config, month, table_name, signal_col_name = args[0], args[1], args[2], args[3]
+    print(f'Merging table {table_name}...')
     s = time.time()
     sig_15s, sig_60s, sig_120s, sig_300s = read_signal_ML(config, month)
     sig_15s = sig_15s[['ticker', 'date', 'time', 'proba']]
@@ -68,8 +69,9 @@ def merge_signal_ensemble(args):
     # write to sql
     lock.acquire()
     s = time.time()
+    print(f'Writing data {month} to SQL...')
     save2sql(table_name, merge_signal)
-    print(f'write {month}, cost {time.time() - s}s')
+    print(f'Finishing writing data in {month}, cost {time.time() - s}s')
     lock.release()
 
 def init(args):
--- train.py
+++ train.py
@@ -63,9 +63,8 @@ def push_signal(opt, suffix=''):
         table_name = f"signal_{pool_name}_lowpriceprice_lgbm_{opt['dataset']['ret_name']}"
     if len(suffix) > 0:
         table_name + f'_{suffix}'
-    print(f'Pushing {table_name} to SQL...')
     write_signals_sql(opt, table_name)
-    print(f'{table_name} has been saved to SQL')
+
 
 
 def main(opt):
@@ -85,4 +84,4 @@ if __name__ == '__main__':
     opt = parse_options(root_path)
     main(opt)
     save_report_disk(opt)
-    push_signal(opt)
+    #push_signal(opt)
