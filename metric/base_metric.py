3b69e319e25d27ffbf7abdcbbeb5ddafaa674d0a
ruanjx <Junxiang.Ruan@cicc.com.cn>
2025-02-17 14:13:35 +0800

    change function name

--- metric/base_metric.py
+++ metric/base_metric.py
@@ -40,7 +40,7 @@ def compute_metric(opt, pre_prob, pre_prob_train, y_test_reg):
     return summary
 
 
-def compute_runtime_metric(opt, pre_prob_train):
+def compute_realtime_metric(opt, pre_prob_train):
     """
     Compute bound, win rate , mean return, total sample .etc as performance
     :param pre_prob_train: train probability
