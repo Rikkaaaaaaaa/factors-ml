8d995d95af68574800515f629d2c8005fae5468e
Yi Wu (EQ) <yi3.wu@cicc.com.cn>
2025-05-13 17:58:07 +0800

    main function to merge main signal and package rt param files

---
+++ merge_and_push_signal_sql.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import glob
+import sys
+sys.path.append('../')
+sys.path.append('./')
+
+from utils.publish import push_single_signal_sql, ensure_table_name
+
+
+def concat_signals_from_folder(signal_path_list):
+    signal = []
+    for r in signal_path_list:
+        signal.append(pd.read_csv(r))
+    return pd.concat(signal)
+
+
+def merge_main_signal(signal_folder_path, upload_month, table_name, database='strategy'):
+    # read all files from path list
+    signal_15s = concat_signals_from_folder(signal_folder_path['signal_15s']).rename(columns={'signal': 'signal_15s'})
+    signal_60s = concat_signals_from_folder(signal_folder_path['signal_60s']).rename(columns={'signal': 'signal_60s'})
+    signal_120s = concat_signals_from_folder(signal_folder_path['signal_120s']).rename(columns={'signal': 'signal_120s'})
+    signal_300s = concat_signals_from_folder(signal_folder_path['signal_300s']).rename(columns={'signal': 'signal_300s'})
+    merge_keys = ['ticker', 'date', 'time']
+    signal_15s = signal_15s[['ticker', 'date', 'time', 'proba']]
+    signal_60s = signal_60s[['ticker', 'date', 'time', 'signal_60s']]
+    signal_120s = signal_120s[['ticker', 'date', 'time', 'signal_120s']]
+    signal_300s = signal_300s[['ticker', 'date', 'time', 'signal_300s']]
+
+    merge_signal = signal_15s.merge(signal_60s, on=merge_keys).merge(signal_120s, on=merge_keys).merge(signal_300s, on=merge_keys)
+    print(f"merge data size is {len(merge_signal)}")
+    # core merge format
+    merge_signal['merge_signal'] = (merge_signal['proba'] - 0.5) * 2 + merge_signal['signal_60s'] * 2 + \
+                              merge_signal['signal_120s'] * 2 + merge_signal['signal_300s'] * 2
+    # filter by upload month
+    if isinstance(upload_month, list):
+        merge_signal = merge_signal[(merge_signal.date<(upload_month[-1]+1)*100) & (merge_signal.date>upload_month[0]*100) ]
+        print(f"Uploading month: {upload_month}")
+        push_single_signal_sql(merge_signal, upload_month=upload_month, table_name=table_name, database=database)
+    else:
+        merge_signal = merge_signal[(merge_signal.date < (upload_month + 1) * 100) & (merge_signal.date > upload_month * 100)]
+        print(f"Uploading month: {upload_month}")
+        push_single_signal_sql(merge_signal, upload_month=[upload_month], table_name=table_name, database=database)
+
+
+def upload_signal_from_ensemble():
+    upload_month = [202404,202405,202406,202407,20208,202409,202410,202411,202412,202501,202502,202503]
+    signal_folder_path = dict()
+    signal_folder_path['signal_15s'] = glob.glob('../ensemble/ensemble_signals/ensemble_with_all_weight_3_1_1_1/*15s.csv')
+    signal_folder_path['signal_60s'] = glob.glob('../ensemble/ensemble_signals/ensemble_with_all_weight_3_1_1_1/*60s.csv')
+    signal_folder_path['signal_120s'] = glob.glob('../ensemble/ensemble_signals/ensemble_with_all_weight_3_1_1_1/*120s.csv')
+    signal_folder_path['signal_300s'] = glob.glob('../ensemble/ensemble_signals/ensemble_manual_selection_300s_weight_3_1_1_1/*300s.csv')
+
+    # table name
+    suffix = 'avg'
+    pool_name = 'hs300'
+    price_level = 'highprice'
+    model_type = 'lgbm'
+    database = 'strategy'
+    table_name = f"ensemble_signal_{pool_name}_{price_level}_{model_type}_{suffix}"
+    ensure_table_name(database, table_name)
+    merge_main_signal(signal_folder_path, upload_month, table_name, database)
+
+
+def upload_signal_from_experiments():
+    upload_month = [202404,202405,202406,202407,202408,202409,202410,202411,202412,202501,202502,202503]
+    # upload_month = [202404]
+    # table name
+    #suffix = 'all_new_selection'
+    suffix = 'all_new_mix2'
+    pool_name = 'hs300'
+    price_level = 'highprice'
+    model_type = 'lgbm'
+    database = 'strategy'
+    table_name = f"ensemble_signal_{pool_name}_{price_level}_{model_type}_{suffix}"
+    signal_folder_path = dict()
+    ensure_table_name(database, table_name)
+    for month in upload_month:
+        signal_folder_path['signal_15s'] = glob.glob(
+            f'experiments/ddb_factor_all_new_hs300_highprice_lgbm_15s/{month}/signal/*.csv')
+        signal_folder_path['signal_60s'] = glob.glob(
+             f'experiments/ddb_factor_all_new_hs300_highprice_lgbm_60s/{month}/signal/*.csv')
+        signal_folder_path['signal_120s'] = glob.glob(
+             f'experiments/ddb_factor_all_new_hs300_highprice_lgbm_120s/{month}/signal/*.csv')
+        signal_folder_path['signal_300s'] = glob.glob(
+            f'ensemble/ensemble_signals/create_mix2_ret_signal/create_mix2_ret_signal_{month}_300s.csv')
+             # f'../experiments/ddb_factor_all_new_manual_selection_hs300_highprice_lgbm_300s/{month}/signal/*.csv')
+
+
+        # append to sql table
+        merge_main_signal(signal_folder_path, month, table_name, database) # use single month, not upload_month
+
+if __name__ == '__main__':
+    upload_signal_from_experiments()
---
+++ pack_realtime_files.py
@@ -0,0 +1,112 @@
+import glob
+from os import makedirs
+import os.path as osp
+import shutil
+import pandas as pd
+
+from utils.option import parse_options
+
+def push_realtime_files(root_path='experiments', exp_name="ddb_factor_hs300_highprice_lgbm" ,res_name='lgbm_data', res_folder_path='./data', test_month=202307):
+    pool_name = 'hs300'
+    highprice_folder = glob.glob(f'{root_path}/{exp_name}*')
+    lowprice_folder = glob.glob(f'{root_path}/{exp_name}*')
+    folders = ['15s', '60s', '120s', '300s']
+    saved_folders = ['ckpt', 'inference_params', 'preprocess_params']
+
+    print(f"Packaging all model files in {test_month}...")
+    for window in folders:
+        window_path = osp.join(res_folder_path, res_name, window)
+        makedirs(window_path)
+        for high_folder in highprice_folder:
+            if window in high_folder and pool_name in high_folder:
+                for folder in saved_folders:
+                    src = osp.join(high_folder, str(test_month), folder)
+                    dst = osp.join(window_path, folder)
+
+                    if not osp.exists(dst):
+                        makedirs(dst)
+                    # 处理preprocess_params文件夹中的文件，增加high/lowprice后缀
+                    if folder == 'preprocess_params':
+                        clip_params = []
+                        std_params = []
+
+                        for clip_csv in glob.glob(f'{src}/clip_*.csv'):
+                            clip_params.append(pd.read_csv(clip_csv))
+                        clip_params = pd.concat(clip_params, ignore_index=True)
+                        clip_path = osp.join(dst, 'clip_params_highprice.csv')
+                        clip_params.to_csv(clip_path, index=False)
+
+                        for std_csv in glob.glob(f'{src}/std_*.csv'):
+                            std_params.append(pd.read_csv(std_csv))
+                        std_params = pd.concat(std_params, ignore_index=True)
+                        std_path = osp.join(dst, 'std_params_highprice.csv')
+                        std_params.to_csv(std_path, index=False)
+
+                    # others: copy all files to new folder
+                    else:
+                        [shutil.copy2(s, dst) for s in glob.glob(src.rstrip('/')+'/*')]
+
+        for low_folder in lowprice_folder:
+            if window in low_folder and pool_name in low_folder:
+                for f in saved_folders:
+                    src = osp.join(low_folder, str(test_month), f)
+                    dst = osp.join(window_path, f)
+                    if not osp.exists(dst):
+                        makedirs(dst)
+
+                    # 处理preprocess_params文件夹中的文件，增加high/lowprice后缀
+                    if f == 'preprocess_params':
+                        clip_params = []
+                        std_params = []
+                        for clip_csv in glob.glob(f'{src}/clip*'):
+                            clip_params.append(pd.read_csv(clip_csv))
+                        clip_params = pd.concat(clip_params, ignore_index=True)
+                        clip_path = osp.join(dst, 'clip_params_lowprice.csv')
+                        clip_params.to_csv(clip_path, index=False)
+
+                        for std_csv in glob.glob(f'{src}/std*'):
+                            std_params.append(pd.read_csv(std_csv))
+                        std_params = pd.concat(std_params, ignore_index=True)
+                        std_path = osp.join(dst, 'std_params_lowprice.csv')
+                        std_params.to_csv(std_path, index=False)
+
+                    else:
+                        [shutil.copy2(s, dst) for s in glob.glob(src.rstrip('/') + '/*')]
+
+    # save factor names
+    save_path = osp.join(res_folder_path, "static_data")
+    if not osp.exists(save_path):
+        makedirs(save_path)
+    factor_name_path = osp.join(save_path, "factor_name.csv")
+    factor_names = get_factor_name_from_15s(exp_name)
+    pd.Series(factor_names, name="factor_name").to_csv(factor_name_path)
+
+    # save history static_data data from sql
+    database = "strategy"
+    for table in ["static_data_industry_hs300", "static_data_industry_zz500", "static_data_industry_zz1000", "static_data_price_hs300","static_data_price_zz500","static_data_price_zz1000"]:
+        table = table + "_history"
+        export_static_data(database, table, test_month, save_path)
+
+    print(f"Suscessfully package all model files in {test_month}...")
+def get_factor_name_from_15s(exp_name):
+    # get factor list of 15s
+    from utils.option import yaml_load
+    from dataset import build_factor_name
+    exp_path = osp.join("experiments", f"{exp_name}_15s/{exp_name}_15s.yaml")
+    opt = yaml_load(exp_path)
+    training_factor_name = build_factor_name(opt['dataset']['training_factor_name'])
+    return training_factor_name
+
+def export_static_data(database, table, test_month, save_path):
+    from utils.mysql import read_table
+    df = read_table(database, f"select * from {table} where test_month={test_month}")
+    df.to_csv(osp.join(save_path, f'{table.replace("_history", "")}.csv'))
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description='package model files for RT')
+    parser.add_argument('--test_month', type=int, default=202412, help="test_month")
+    parser.add_argument('--exp_name', type=str, default="ddb_factor_hs300_highprice_lgbm", help="experiment name")
+    config = parser.parse_args()
+    push_realtime_files(exp_name=config.exp_name, test_month=config.test_month)
