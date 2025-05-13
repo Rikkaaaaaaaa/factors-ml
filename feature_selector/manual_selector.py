f65f955a151b3169b2ff9700413c0a436b676905
Yi Wu (EQ) <yi3.wu@cicc.com.cn>
2025-05-13 17:48:50 +0800

    add manual selector

--- feature_selector/manual_selector.py
+++ feature_selector/manual_selector.py
@@ -21,29 +21,21 @@ class ManualSelector():
 
     def select_factor(self, train, target_train):
         self.logger.info(f"{self.test_month}_indus_{self.indus_type}: Start manual factor selection...")
-        # self.max_features = min(train.shape[1], self.opt['feature_selector']['selected_feature_num'])
-        # if self.max_features in [150, 200, 250]:
-        #     selected_factor = pd.read_csv('feature_selector/selected_factors_{}_corr85.csv'.format(self.max_features))['factor_name'].tolist()
-        # else:
-        #     selected_factor = self.training_factor_name
-        #     self.logger.info(f"{self.test_month}_indus_{self.indus_type}: !!! no manual factor file")
         if self.opt["feature_selector"].get("selected_factor_file_name"):
             selected_factor_file_name = self.opt["feature_selector"].get("selected_factor_file_name")
             file_path = f'feature_selector/selected_factor_names/{selected_factor_file_name}'
             if os.path.exists(file_path):
                 selected_factor = pd.read_csv(file_path)['factor_name'].tolist()
-                self.logger.info(f"{self.test_month}_indus_{self.indus_type}: There is {len(selected_factor)} selected factors")
+                self.logger.info(f"{self.test_month}_indus_{self.indus_type}: There are {len(selected_factor)} selected factors in {file_path}")
             else:
                 error_info = f"{self.test_month}_indus_{self.indus_type}: There is no selected factor name file in {file_path}!"
                 self.logger.info(error_info)
                 raise FileNotFoundError(error_info)
-
         else:
             error_info = f"{self.test_month}_indus_{self.indus_type}: There is no key named 'selected_factor_file_name' in option.feature_selector"
             self.logger.info(error_info)
             raise AttributeError(error_info)
 
-
-        self.logger.info(f"{self.test_month}_indus_{self.indus_type}: Finish factor selection with feature num={self.max_features}")
+        self.logger.info(f"{self.test_month}_indus_{self.indus_type}: Finish factor selection with feature num={len(selected_factor)}")
 
         return selected_factor
