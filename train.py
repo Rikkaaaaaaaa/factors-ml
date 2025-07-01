02df1b303b0e62e97851bdd3b712a518439f0b60
Yi Wu (EQ) <yi3.wu@cicc.com.cn>
2025-07-01 11:14:58 +0800

    add terminal info of indus_list to avoid error

--- train.py
+++ train.py
@@ -55,7 +55,7 @@ def init_args(opt):
     args = []
     for test_month in opt['dataset']['test_month']:
         industry = check_indus(opt, test_month)
-        # industry = [10]
+        print(f"Including industry: {industry}!")
         for indus_type in industry:
             if not exists_results(opt, test_month, indus_type):
                 args.append((opt, test_month, indus_type))
