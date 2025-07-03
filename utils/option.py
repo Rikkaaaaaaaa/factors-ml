c21d3413a7f675b16a01e2e55e764ddde1ae1773
Yi Wu (EQ) <yi3.wu@cicc.com.cn>
2025-07-03 16:06:49 +0800

    delete dulplicated line

--- utils/option.py
+++ utils/option.py
@@ -78,7 +78,7 @@ def yaml_load(f):
 
 def parse_options(root_path, ensure=True):
     parser = argparse.ArgumentParser()
-    parser.add_argument('-option', type=str, default='option/prod_train/prod_ddb_null_factor_no_reverse_hs300_highprice_lgbm_15s.yaml', help='Path to option YAML file.')
+    parser.add_argument('-option', type=str, default='option/ddb_null_factor_reg/ddb_null_factor_reg_hs300_highprice_lgbm_15s.yaml', help='Path to option YAML file.')
     parser.add_argument('-is_realtime', action='store_true', help='Whether the phase is backtesting or realtime')
     parser.add_argument('-debug', action='store_true', help='Whether to use debug mode') # it'll contain ticker num <= 10
     args = parser.parse_args()
