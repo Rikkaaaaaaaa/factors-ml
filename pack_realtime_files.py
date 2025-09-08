248a44c7d23189b72f08029792b56b11d8d4d0c0
Yi Wu (EQ) <yi3.wu@cicc.com.cn>
2025-09-08 15:31:49 +0800

    1. pack threshold pct num 2. add low price

--- pack_realtime_files.py
+++ pack_realtime_files.py
@@ -2,16 +2,41 @@ import glob
 from os import makedirs
 import os.path as osp
 import shutil
+import numpy as np
 import pandas as pd
+import os
 
 from utils.option import parse_options
 
-def push_realtime_files(root_path='experiments', exp_name="ddb_factor_hs300_highprice_lgbm" ,res_name='lgbm_data', res_folder_path='./data', test_month=202307):
+def save_threshold_pct(train_proba_root_path, output_path):
+    # read and merge all train proba
+    train_signal = []
+    for train_signal_path in sorted(os.listdir(train_proba_root_path)):
+        train_signal.append(pd.read_csv((os.path.join(train_proba_root_path, train_signal_path))))
+    train_proba = pd.concat(train_signal)
+    train_proba = train_proba.sort_values(by=['ticker'])
+
+    # define pct num list
+    pct_num = [1, 3,5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 97, 99]
+    threshold_df = pd.DataFrame()
+    for pct in pct_num:
+        pct_values = train_proba.groupby('ticker')['proba'].apply(lambda x: np.percentile(x, pct)).reset_index().rename(columns={'proba': f'pct_{pct}'})
+        if threshold_df.empty:
+            threshold_df = pct_values
+        else:
+            threshold_df = pd.merge(threshold_df, pct_values, on=['ticker'])
+
+    # save to csv file
+    threshold_df.to_csv(output_path)
+
+    return
+
+
+def push_realtime_files(root_path='experiments', exp_name="ddb_factor_hs300_highprice_lgbm",res_name='lgbm_data', res_folder_path='./data', test_month=202307):
     pool_name = 'hs300'
     highprice_folder = glob.glob(f'{root_path}/{exp_name}*')
-    lowprice_folder = glob.glob(f'{root_path}/{exp_name}*')
     folders = ['15s', '60s', '120s', '300s']
-    saved_folders = ['ckpt', 'inference_params', 'preprocess_params']
+    saved_folders = ['ckpt', 'inference_params', 'preprocess_params', 'train_signal']
 
     print(f"Packaging all model files in {test_month}...")
     for window in folders:
@@ -41,37 +66,20 @@ def push_realtime_files(root_path='experiments', exp_name="ddb_factor_hs300_high
                         std_params = pd.concat(std_params, ignore_index=True)
                         std_path = osp.join(dst, 'std_params_highprice.csv')
                         std_params.to_csv(std_path, index=False)
+                        print(f"[Processing preprocessing params] Clip and std params have been saved at {clip_path} and {std_path}")
+
+                    # compute and save threshold_pct to csv
+                    elif folder == 'train_signal':
+
+                        output_path = osp.join(dst, 'threshold_pct.csv')
+                        save_threshold_pct(src, output_path)
+                        print(f"[Processing threshold pct] Threshold_pct has been computed and saved at {output_path}")
 
                     # others: copy all files to new folder
                     else:
                         [shutil.copy2(s, dst) for s in glob.glob(src.rstrip('/')+'/*')]
+                        print(f"[Processing {folder}] Files in {src} has been copied to {dst}")
 
-        for low_folder in lowprice_folder:
-            if window in low_folder and pool_name in low_folder:
-                for f in saved_folders:
-                    src = osp.join(low_folder, str(test_month), f)
-                    dst = osp.join(window_path, f)
-                    if not osp.exists(dst):
-                        makedirs(dst)
-
-                    # 处理preprocess_params文件夹中的文件，增加high/lowprice后缀
-                    if f == 'preprocess_params':
-                        clip_params = []
-                        std_params = []
-                        for clip_csv in glob.glob(f'{src}/clip*'):
-                            clip_params.append(pd.read_csv(clip_csv))
-                        clip_params = pd.concat(clip_params, ignore_index=True)
-                        clip_path = osp.join(dst, 'clip_params_lowprice.csv')
-                        clip_params.to_csv(clip_path, index=False)
-
-                        for std_csv in glob.glob(f'{src}/std*'):
-                            std_params.append(pd.read_csv(std_csv))
-                        std_params = pd.concat(std_params, ignore_index=True)
-                        std_path = osp.join(dst, 'std_params_lowprice.csv')
-                        std_params.to_csv(std_path, index=False)
-
-                    else:
-                        [shutil.copy2(s, dst) for s in glob.glob(src.rstrip('/') + '/*')]
 
     # save factor names
     save_path = osp.join(res_folder_path, "static_data")
@@ -80,14 +88,16 @@ def push_realtime_files(root_path='experiments', exp_name="ddb_factor_hs300_high
     factor_name_path = osp.join(save_path, "factor_name.csv")
     factor_names = get_factor_name_from_15s(exp_name)
     pd.Series(factor_names, name="factor_name").to_csv(factor_name_path)
+    print(f"[Processing factor names] Factor names in 15s have been copied to {factor_name_path}")
 
     # save history static_data data from sql
     database = "strategy"
     for table in ["static_data_industry_hs300", "static_data_industry_zz500", "static_data_industry_zz1000", "static_data_price_hs300","static_data_price_zz500","static_data_price_zz1000"]:
         table = table + "_history"
         export_static_data(database, table, test_month, save_path)
+    print(f"[Processing static data] Static data in MySQL have been saved at {save_path}")
 
-    print(f"Suscessfully package all model files in {test_month}...")
+    print(f"Suscessfully package all model files in {test_month} to {res_folder_path}")
 def get_factor_name_from_15s(exp_name):
     # get factor list of 15s
     from utils.option import yaml_load
@@ -97,16 +107,73 @@ def get_factor_name_from_15s(exp_name):
     training_factor_name = build_factor_name(opt['dataset']['training_factor_name'])
     return training_factor_name
 
+
 def export_static_data(database, table, test_month, save_path):
     from utils.mysql import read_table
     df = read_table(database, f"select * from {table} where test_month={test_month}")
     df.to_csv(osp.join(save_path, f'{table.replace("_history", "")}.csv'), encoding='utf-8')
 
 
+def push_realtime_files_low_price(root_path='experiments', exp_name="low_price_hs300_logit", res_name='logit_data', res_folder_path='./data',
+                                  test_month=202508, pool_name="hs300"):
+    low_folder = osp.join(root_path, exp_name)
+    saved_folders = ['ckpt', 'inference_params', 'preprocess_params']
+
+    print(f"Packaging all model files in {test_month}...")
+
+    # target_path = osp.join(res_folder_path, pool_name, res_name)  # TODO
+    target_path = osp.join(res_folder_path, res_name)
+    makedirs(target_path)
+
+    if pool_name in low_folder:
+        for folder in saved_folders:
+            src = osp.join(low_folder, str(test_month), folder)
+            dst = osp.join(target_path, folder)
+
+            if not osp.exists(dst):
+                makedirs(dst)
+            # 处理preprocess_params文件夹中的文件，增加high/lowprice后缀
+            if folder == 'preprocess_params':
+                clip_params = []
+                std_params = []
+
+                for clip_csv in glob.glob(f'{src}/clip_*.csv'):
+                    clip_params.append(pd.read_csv(clip_csv))
+                clip_params = pd.concat(clip_params, ignore_index=True)
+                clip_path = osp.join(dst, 'clip_params_lowprice.csv')
+                clip_params.to_csv(clip_path, index=False)
+
+                for std_csv in glob.glob(f'{src}/std_*.csv'):
+                    std_params.append(pd.read_csv(std_csv))
+                std_params = pd.concat(std_params, ignore_index=True)
+                std_path = osp.join(dst, 'std_params_lowprice.csv')
+                std_params.to_csv(std_path, index=False)
+
+            # others: copy all files to new folder
+            else:
+                for bs_flag in ['b', 's']:
+                    dst_with_bs_flag = osp.join(dst, bs_flag)
+                    src_with_bs_flag = osp.join(src, bs_flag)
+                    makedirs(dst_with_bs_flag)
+                    [shutil.copy2(s, dst_with_bs_flag) for s in glob.glob(src_with_bs_flag.rstrip('/')+'/*')]
+
+    # save factor names
+    save_path = osp.join(res_folder_path, "static_data")
+    if not osp.exists(save_path):
+        makedirs(save_path)
+    factor_name_path = osp.join(save_path, "factor_name_low_price.csv")
+    factor_name_src_path = osp.join(low_folder, str(test_month), "factor_name_low_price.csv")
+    shutil.copy2(factor_name_src_path, factor_name_path)
+    print(f"[Processing factor names low price] Factor names for low price have been copied to {factor_name_path}")
+
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='package model files for RT')
-    parser.add_argument('--test_month', type=int, default=202412, help="test_month")
-    parser.add_argument('--exp_name', type=str, default="ddb_factor_hs300_highprice_lgbm", help="experiment name")
+    parser.add_argument('--test_month', type=int, default=202509, help="test_month")
+    parser.add_argument('--pool_name', type=str, default="hs300", help="test_month")
+    parser.add_argument('--exp_name', type=str, default="prod_202509_ddb_null_factor_no_reverse_hs300_highprice_lgbm", help="experiment name")
+    parser.add_argument('--exp_name_low_price', type=str, default="low_price_hs300_rt", help="experiment name low price")
     config = parser.parse_args()
     push_realtime_files(exp_name=config.exp_name, test_month=config.test_month)
+    push_realtime_files_low_price(exp_name=config.exp_name_low_price, test_month=config.test_month, pool_name=config.pool_name)
