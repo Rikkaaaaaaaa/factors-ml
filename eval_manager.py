e6997ad0cb7876a1fe84c6b3ce16af22e22417c2
Yi Wu (EQ) <yi3.wu@cicc.com.cn>
2025-12-08 15:26:13 +0800

    update option manager

---
+++ eval_manager.py
@@ -0,0 +1,133 @@
+import multiprocessing as mp
+import os.path as osp
+import logging
+import argparse
+
+from dataset.sql_ops import check_indus
+from dataset import build_dataset
+from models import build_model
+from feature_selector import build_selector
+from backtester import BackTester
+from utils.logger import get_root_logger, get_env_info
+from utils.option import parse_options, parse_opt_manager, dict2str
+from utils.publish import save_report_disk, push_signal_sql
+from utils.misc import Timer, time_str, get_time_str, exists_results, ensure_path
+
+
+
+def init_lock(l):
+    global dataset_lock
+    dataset_lock = l
+
+def train_pipeline(train_args):
+    opt_manager, test_month, indus_type, train_sub_option_names = train_args
+    cache_data = None
+    for sub_opt_name in opt_manager['sub_options'].keys():
+        # filter option to avoid overwrite experiment results
+        if not sub_opt_name in train_sub_option_names:
+            continue
+        opt = opt_manager['sub_options'][sub_opt_name]
+        # logger init
+        logger_name = f"month{test_month}_indus{indus_type}"
+        log_file = osp.join(opt['path']['log'], f"{logger_name}_{get_time_str()}.log")
+        logger = get_root_logger(logger_name=logger_name, log_level=logging.INFO, log_file=log_file)
+        logger.info(f"[{logger_name}] Traning data return name is {sub_opt_name}")
+        #logger.info(f"[{logger_name}] {dict2str(opt)}")
+
+        # define task type
+        task_type = opt['dataset'].setdefault('task_type', 'classification')
+        if task_type == 'classification':
+            label_col_name = 'class_label'
+        else:
+            label_col_name = 'ret'
+
+        # get data set from test month
+        dataset = build_dataset(opt, test_month=test_month, indus_type=indus_type, logger_name=logger_name)
+
+        # cache data
+        load_timer = Timer()
+        load_timer.start()
+        if cache_data is None:
+            logger.info(f"[{logger_name}] Loading factor and return from database")
+            dataset_lock.acquire()
+            cache_data = dataset.load_data(cache=True)
+            dataset_lock.release()
+            logger.info(f"[{logger_name}] Suscessfully Load data from [database] with cost={time_str(load_timer.item())}")
+            logger.info(f"[{logger_name}] Suscessfully cache data in memory")
+        else:
+            logger.info(f"[{logger_name}] Loading factor and return from cache data")
+            dataset.load_data(cache=False, data=cache_data)
+            logger.info(f"[{logger_name}] Suscessfully load data from [cache data] with cost={time_str(load_timer.item())}")
+        if dataset.is_empty:
+            logger.info(f"[{logger_name}] Dataset is empty!")
+            return
+
+        # # factor selection
+        # training_factor_name = dataset.training_factor_name
+        # if opt.get('feature_selector'):
+        #     feature_selector = build_selector(opt, test_month=test_month, indus_type=indus_type)
+        #     selected_factor_name = feature_selector.select_factor(x_train, y_train)
+        #     dataset.set_selected_factor(selected_factor_name)
+        #     training_factor_name = dataset.selected_factor_name
+        #     logger.info(f"[{logger_name}] Applying feature selection, total training factor num is: {len(training_factor_name)}")
+
+        # eval model
+        model = build_model(opt, test_month=test_month, indus_type=indus_type)
+        model.load_ckpt()
+
+        # backtesting or record inference_bound
+        backtester = BackTester(opt, test_month, indus_type, logger_name=logger_name)
+        backtester.backtest_eval(dataset, model)
+
+def init_args(opt_manager):
+    args = []
+    for test_month in opt_manager['dataset']['test_month']:
+        industry = check_indus(opt_manager, test_month)
+        print(
+            f"Loading indus by [{opt_manager['dataset']['indus_class']}] from table [static_data_industry_{opt_manager['dataset']['pool_name']}_history] + [{opt_manager['dataset']['indus_table_suffix']}]]")
+        print(f"Including industry id: {industry}")
+        for indus_type in industry:
+            # check whether any result of sub options doesn't exist
+            train_sub_option_names = []
+            for sub_opt_name in opt_manager['sub_options'].keys():
+                sub_opt = opt_manager['sub_options'][sub_opt_name]
+                if not exists_results(sub_opt, test_month, indus_type) or opt_manager['mode']=='eval':
+                    # if not exists one of sub options, append to args
+                    train_sub_option_names.append(sub_opt_name)
+            args.append((opt_manager, test_month, indus_type, train_sub_option_names))
+
+    return args
+
+
+def main(opt_manager):
+    # mp training
+    manager = mp.Manager()
+    dataset_lock = manager.Lock()
+    pool = mp.Pool(processes=opt_manager['n_jobs'], initializer=init_lock, initargs=(dataset_lock,))
+    global_timer = Timer()
+    args = init_args(opt_manager)
+    results = [pool.apply_async(train_pipeline, (arg,)) for arg in args]
+    [result.get() for result in results]
+    pool.close()
+    pool.join()
+    print("Task time is {}".format(time_str(global_timer.item())))
+    # save report
+    if not opt_manager['is_realtime']:
+        for sub_opt_name in opt_manager['sub_options'].keys():
+            opt = opt_manager['sub_options'][sub_opt_name]
+            save_report_disk(opt)
+
+
+if __name__ == '__main__':
+    print(get_env_info())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-root_path', type=str, default='./', help='Root path of project.')
+    parser.add_argument('-option', type=str, default='option/eval/opt_manager/hs300_test/am_model/am_model_hs300_highprice_lgbm_opt_manager.yaml', help='Path to option YAML file.')
+    parser.add_argument('-is_realtime', action='store_true', help='Whether the phase is backtesting or realtime')
+    parser.add_argument('-debug', action='store_true', help='Whether to use debug mode') # it'll contain ticker num <= 10
+    args = parser.parse_args()
+    opt_manager = parse_opt_manager(args)
+    main(opt_manager)
+
+
+
--- utils/option.py
+++ utils/option.py
@@ -6,7 +6,7 @@ import argparse
 import os.path as osp
 import shutil
 
-from utils.misc import set_random_seed, ensure_path, mkdir
+from utils.misc import set_random_seed, ensure_path, mkdir, deep_merge_with_log
 
 def dict2str(opt, indent_level=1):
     """dict to string for printing options.
@@ -76,30 +76,36 @@ def yaml_load(f):
     #     return yaml.load(f, Loader=yaml.FullLoader)
 
 
-def parse_options(root_path, ensure=True, yaml_path=None):
-    if yaml_path:
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-option', type=str, default=os.path.join(root_path, yaml_path), help='Path to option YAML file.')
-        parser.add_argument('-is_realtime', action='store_true', help='Whether the phase is backtesting or realtime')
-        parser.add_argument('-debug', action='store_true',
-                            help='Whether to use debug mode')  # it'll contain ticker num <= 10
-        args = parser.parse_args()
-    else:
-        parser = argparse.ArgumentParser()
-        parser.add_argument('-option', type=str, default='option/ddb_null_factor_reg/ddb_null_factor_reg_hs300_highprice_lgbm_15s.yaml', help='Path to option YAML file.')
-        parser.add_argument('-is_realtime', action='store_true', help='Whether the phase is backtesting or realtime')
-        parser.add_argument('-debug', action='store_true', help='Whether to use debug mode') # it'll contain ticker num <= 10
-        args = parser.parse_args()
-
+def parse_options(args, ensure=True, yaml_path=None, opt_manager=None):
     # parse yml to dict
-    if not osp.exists(args.option):
+    if not yaml_path:
+        yaml_path = args.option
+    if not osp.exists(yaml_path):
         raise FileExistsError(f"No such option file named", args.option)
-    opt = yaml_load(args.option)
+    opt = yaml_load(yaml_path)
 
-    # parse cmd flag
+    # parse argparser
     if not opt.get('is_realtime'):
         opt['is_realtime'] = args.is_realtime
     opt['debug'] = args.debug
+    root_path = args.root_path
+
+    # get mode param
+    opt['mode'] = opt.get('mode', 'train')
+    if opt['is_realtime']:
+        opt['mode'] = 'rt'
+
+    # overwrite option with option manager
+    if not opt_manager is None:
+        # opt['dataset']['test_month'] = opt_manager['test_month']
+        # opt['dataset']['pool_name'] = opt_manager['pool_name']
+        # opt['base_name'] = opt_manager['base_name']
+        # opt['mode'] = opt_manager['mode']
+        merged_opt, changes = deep_merge_with_log(opt, opt_manager)
+        print(f"[option_{opt['dataset']['ret_name']}] Suscessfully update option with option manager:")
+        for key, old_val, new_val in changes:
+            print(f"  {key}: {old_val} -> {new_val}")
+        opt = merged_opt
 
     # random seed
     seed = opt.get('manual_seed')
@@ -111,19 +117,40 @@ def parse_options(root_path, ensure=True, yaml_path=None):
     # save path init
     if not opt.get('path'):
         opt['path'] = dict()
+    # root_path
+        opt['path']['root_path'] = root_path
+
+    # base name
+    if opt.get('base_name'):
+        exp_folder_name = f"{opt['base_name']}_{opt['dataset']['ret_name']}"
+        opt['name'] = exp_folder_name
+    else:
+        # adjust for old option file with param "name"
+        if opt.get("name"):
+            exp_folder_name = opt['name']
+        else:
+            raise ValueError('option file need param named [base_name]!')
+    print(f"Running {opt['mode']} mode in {opt['name']}")
 
     # experiment path
     experiments_root = opt['path'].get('experiments_root')
     if experiments_root is None:
-        experiments_root = osp.join(root_path, 'experiments')
         # eval experiments path
-        if  opt['eval_rt']:
-            experiments_root = osp.join(root_path, 'eval_experiments')
-    experiments_root = osp.join(experiments_root, opt['name'])
-    opt['path']['experiments_root'] = experiments_root
-    if ensure:
-        ensure_path(experiments_root)
+        if opt['mode'] == 'eval':
+            experimens_root_path = 'eval_experiments'
+        else:
+            experimens_root_path = 'experiments'
+        # base name folder
+        if opt.get('base_name'):
+            experiments_root = osp.join(root_path, experimens_root_path, opt['base_name'], exp_folder_name)
+        else:
+            experiments_root = osp.join(root_path, experimens_root_path, exp_folder_name)
+        opt['path']['experiments_root'] = experiments_root
+    print(f"Files will be save at experiment folder path [{experiments_root}]")
 
+    # ensure path
+    if opt['mode'] != 'eval':
+        opt['new_session_flag'] = ensure_path(experiments_root, ensure)     # opt['new_session_flag']=True if input "y"
 
     # saving path
     opt['path']['model_path'] = dict()
@@ -166,6 +193,29 @@ def parse_options(root_path, ensure=True, yaml_path=None):
             if not os.path.exists(selection_path):
                 os.makedirs(selection_path)
 
+    if opt['mode'] == 'eval':
+        # define pretrain param path
+        # base name folder
+        if opt.get('base_name'):
+            pretrain_root = osp.join(root_path, 'experiments', opt['base_name'], exp_folder_name)
+        else:
+            pretrain_root = osp.join(root_path, 'experiments', exp_folder_name)
+        print(f"Loading pretrained model from {pretrain_root}")
+
+        opt['path']['pretrain_root'] = pretrain_root
+        opt['path']['pretrain_model_path'] = dict()
+        opt['path']['pretrain_preprocess_path'] = dict()
+        opt['path']['pretrain_inference_path'] = dict()
+        for test_month in opt['dataset']['test_month']:
+            model_path = osp.join(pretrain_root, str(test_month), 'ckpt')
+            opt['path']['pretrain_model_path'][test_month] = model_path
+
+            preprocess_path = osp.join(pretrain_root, str(test_month), 'preprocess_params')
+            opt['path']['pretrain_preprocess_path'][test_month] = preprocess_path
+
+            inference_path = osp.join(pretrain_root, str(test_month), 'inference_params')
+            opt['path']['pretrain_inference_path'][test_month] = inference_path
+
 
     # log path
     log_root = opt['path'].get('log_root')
@@ -175,7 +225,60 @@ def parse_options(root_path, ensure=True, yaml_path=None):
     mkdir(log_root)
 
     # copy option
-    shutil.copy2(args.option, opt['path']['experiments_root'])
+    shutil.copy2(yaml_path, opt['path']['experiments_root'])
 
-    return opt, args
+    return opt
+def parse_opt_manager(args, ensure=True, yaml_path=None):
+    # parse yml to dict
+    if not yaml_path:
+        yaml_path = args.option
+    if not osp.exists(yaml_path):
+        raise FileExistsError(f"No such option file named", args.option)
+    opt_manager = yaml_load(yaml_path)
 
+    # parse argparser
+    if not opt_manager.get('is_realtime'):
+        opt_manager['is_realtime'] = args.is_realtime
+    opt_manager['debug'] = args.debug
+    root_path = args.root_path
+
+    # get mode param
+    opt_manager['mode'] = opt_manager.get('mode', 'train')
+    if opt_manager['is_realtime']:
+        opt_manager['mode'] = 'rt'
+    print(f"[option manager] option manager [{opt_manager['base_name']} has been created]")
+    print(f"[option manager] Running {opt_manager['mode']} mode with option manager")
+    print(f"[option manager] Return windows include [{opt_manager['ret_names']}]")
+
+    # random seed
+    seed = opt_manager.get('manual_seed')
+    if seed is None:
+        seed = random.randint(1, 10000)
+        opt_manager['manual_seed'] = seed
+    set_random_seed(seed)
+
+    # experiment path
+    experiments_root = osp.join(root_path, 'experiments')
+    # eval experiments path
+    if opt_manager['mode'] == 'eval':
+        experiments_root = osp.join(root_path, 'eval_experiments')
+    experiments_root = osp.join(experiments_root, opt_manager['base_name'])
+    opt_manager['experiments_root'] = experiments_root
+    # ensure experiments_root
+    if opt_manager['mode'] != 'eval':
+        ensure_path(experiments_root)
+    print(f"[opt manager] Suscessfully copied all yaml files to {experiments_root}")
+
+    # sub options init
+    opt_manager['opt_folder_path'] = os.path.dirname(args.option)
+    sub_options = {}
+    # update multi return option, such as xxx_hs300_highprice_lgbm_15s.yaml
+    for ret_name in opt_manager['ret_names']:
+        sub_opt_path = os.path.join(opt_manager['opt_folder_path'], f"{opt_manager['base_name']}_{ret_name}.yaml")
+        sub_options[ret_name] = parse_options(args, ensure=False, yaml_path=sub_opt_path, opt_manager=opt_manager)
+    opt_manager['sub_options'] = sub_options
+
+    # copy opt_manager
+    shutil.copy2(yaml_path, opt_manager['experiments_root'])
+
+    return opt_manager
