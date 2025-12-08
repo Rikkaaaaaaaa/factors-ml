591322888d79b9974243474d2bc88778433d63f4
Yi Wu (EQ) <yi3.wu@cicc.com.cn>
2025-12-08 15:29:16 +0800

    add eval mode in merge_signal_by_pct2

--- merge_main_signal/create_main_signal.py
+++ merge_main_signal/create_main_signal.py
@@ -59,11 +59,17 @@ def merge_signal_by_pct(expr_name, ret_name, month):
 
     return signal[["ticker", "date", "time", "new_signal"]]
 
-def merge_signal_by_pct2(expr_name, ret_name, month):
-    expr_path = f"../experiments/{expr_name}"
+def merge_signal_by_pct2(expr_name, ret_name, month, eval=False):
+    if eval:
+        expr_path = f"../eval_experiments/{expr_name}"
+        train_expr_path = expr_path.replace('eval_experiments', 'experiments')
+    else:
+        expr_path = f"../experiments/{expr_name}"
+        train_expr_path = expr_path
+
     signal_path = f"{expr_path}/{month}/signal"
     signal = read_signal_from_csv(signal_path) # keep data when return=0
-    train_proba = read_train_signal_from_csv(f"{expr_path}/{month}/train_signal")
+    train_proba = read_train_signal_from_csv(f"{train_expr_path}/{month}/train_signal")
     train_proba = train_proba.sort_values(by=['ticker'])
 
     pct_num = [1, 3,5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 97, 99]
@@ -97,6 +103,53 @@ def merge_signal_by_pct2(expr_name, ret_name, month):
     check_res = check_dist(signal, "new_signal")
 
     return signal[["ticker", "date", "time", "new_signal"]]
+
+
+def merge_signal_by_pct2_930_940_15s_signal(expr_name, ret_name, month, eval=False):
+    if eval:
+        expr_path = f"../eval_experiments/{expr_name}"
+        train_expr_path = expr_path.replace('eval_experiments', 'experiments')
+    else:
+        expr_path = f"../experiments/{expr_name}"
+        train_expr_path = expr_path
+
+    signal_path = f"{expr_path}/{month}/signal"
+    signal = read_signal_from_csv(signal_path) # keep data when return=0
+    train_proba = read_train_signal_from_csv(f"{train_expr_path}/{month}/train_signal")
+    train_proba = train_proba.sort_values(by=['ticker'])
+
+    pct_num = [1, 3,5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 97, 99]
+    for pct in pct_num:
+        threshold = train_proba.groupby('ticker')['proba'].apply(lambda x: np.percentile(x, pct)).reset_index().rename(columns={'proba': f'pct_{pct}'})
+        signal = signal.merge(threshold, on=['ticker'])
+
+    if ret_name in ['60s', '120s', '300s']:
+        new_signal = np.zeros(len(signal['signal']))
+        proba = signal['proba'].values
+        # up signal
+        new_signal[proba > signal['pct_80'].values] = 0.8
+        new_signal[proba > signal['pct_90'].values] = 1.1
+        new_signal[proba > signal['pct_95'].values] = 1.2
+        new_signal[proba > signal['pct_97'].values] = 1.3
+        new_signal[proba > signal['pct_99'].values] = 1.4
+        # down signal
+        new_signal[proba < signal['pct_20'].values] = -0.8
+        new_signal[proba < signal['pct_10'].values] = -1.1
+        new_signal[proba < signal['pct_5'].values] = -1.2
+        new_signal[proba < signal['pct_3'].values] = -1.3
+        new_signal[proba < signal['pct_1'].values] = -1.4
+        signal['new_signal'] = new_signal
+
+    if ret_name == '15s':
+        new_signal = np.zeros(len(signal['signal']))
+        proba = signal['proba'].values
+        # up signal
+        new_signal = proba - signal['pct_50'].values
+        signal['new_signal'] = new_signal
+    check_res = check_dist(signal, "new_signal")
+    signal.loc[signal['time']<=94000000,'new_signal'] = signal.loc[signal['time']<=94000000,'signal']
+    return signal[["ticker", "date", "time", "new_signal"]]
+
 
 def read_signal_and_adjust_main_signal_enhance(expr_name, ret_name, month):
     expr_path = f"../experiments/{expr_name}"
@@ -174,7 +227,7 @@ def merge_bond_signal(expr_name, ret_name, month):
 
     return signal[["ticker", "date", "time", "new_signal"]]
 
-def ensemble_all_new_6m(months, trading_hours=None):
+def ensemble_all_new_6m(months, trading_hours=None, eval=False):
     try:
         # table name
         suffix = 'all_new_pct2'
@@ -195,10 +248,10 @@ def ensemble_all_new_6m(months, trading_hours=None):
             # expr_name_mix_120s = 'mix3_1_1_1_pct20_hs300_highprice_lgbm_120s'
             # expr_name_mix_300s = 'mix4_1_1_1_1_pct20_hs300_highprice_lgbm_300s'
 
-            signal_15s = merge_signal_by_pct2(expr_name_15s, '15s', month).rename(columns={'new_signal':'new_signal_15s'})
-            signal_60s = merge_signal_by_pct2(expr_name_60s, '60s', month).rename(columns={'new_signal':'new_signal_60s'})
-            signal_120s = merge_signal_by_pct2(expr_name_120s, '120s',month).rename(columns={'new_signal':'new_signal_120s'})
-            signal_300s = merge_signal_by_pct2(expr_name_300s, '300s', month).rename(columns={'new_signal':'new_signal_300s'})
+            signal_15s = merge_signal_by_pct2(expr_name_15s, '15s', month, eval).rename(columns={'new_signal':'new_signal_15s'})
+            signal_60s = merge_signal_by_pct2(expr_name_60s, '60s', month, eval).rename(columns={'new_signal':'new_signal_60s'})
+            signal_120s = merge_signal_by_pct2(expr_name_120s, '120s',month, eval).rename(columns={'new_signal':'new_signal_120s'})
+            signal_300s = merge_signal_by_pct2(expr_name_300s, '300s', month, eval).rename(columns={'new_signal':'new_signal_300s'})
 
             merge_keys = ['ticker', 'date', 'time']
             merge_signal = signal_15s.merge(signal_60s, on=merge_keys).merge(signal_120s, on=merge_keys).merge(signal_300s, on=merge_keys)
@@ -267,15 +320,15 @@ def ensemble_all_new_6m_enhance(months, trading_hours=None):
     except Exception as e:
         print(e)
 
-def ensemble_batch3_pct(months, del_month=True, trading_hours=None):
-    trading_hours = {
-        "start_time": 130000,
-        "end_time": 145700,
-    }
+def ensemble_batch3_pct(months, del_month=True, trading_hours=None, eval=False):
+    # trading_hours = {
+    #     "start_time": 93000,
+    #     "end_time": 113000,
+    # }
     try:
         # table name
-        suffix = 'batch3_pct2_mix_am_pm'
-        pool_name = 'hs300'
+        suffix = 'batch3_pct2'
+        pool_name = 'zz500'
         price_level = 'highprice'
         model_type = 'lgbm'
         database = 'strategy'
@@ -284,18 +337,18 @@ def ensemble_batch3_pct(months, del_month=True, trading_hours=None):
 
         for month in months:
             # read signal and train proba
-            expr_name_15s = f'ddb_null_factor_no_reverse_with_930_940_{pool_name}_highprice_lgbm_15s'
-            expr_name_60s = f'ddb_null_factor_no_reverse_with_930_940_{pool_name}_highprice_lgbm_60s'
-            expr_name_120s = f'ddb_null_factor_no_reverse_with_930_940_{pool_name}_highprice_lgbm_120s'
-            expr_name_300s = f'ddb_null_factor_no_reverse_with_930_940_{pool_name}_highprice_lgbm_300s'
+            expr_name_15s = f'ddb_null_factor_no_reverse_{pool_name}_highprice_lgbm_15s'
+            expr_name_60s = f'ddb_null_factor_no_reverse_{pool_name}_highprice_lgbm_60s'
+            expr_name_120s = f'ddb_null_factor_no_reverse_{pool_name}_highprice_lgbm_120s'
+            expr_name_300s = f'ddb_null_factor_no_reverse_{pool_name}_highprice_lgbm_300s'
             # expr_name_mix_60s = 'mix2_1_1_pct20_hs300_highprice_lgbm_60s'
             # expr_name_mix_120s = 'mix3_1_1_1_pct20_hs300_highprice_lgbm_120s'
             # expr_name_mix_300s = 'mix4_1_1_1_1_pct20_hs300_highprice_lgbm_300s'
 
-            signal_15s = merge_signal_by_pct2(expr_name_15s, '15s', month).rename(columns={'new_signal':'new_signal_15s'})
-            signal_60s = merge_signal_by_pct2(expr_name_60s, '60s', month).rename(columns={'new_signal':'new_signal_60s'})
-            signal_120s = merge_signal_by_pct2(expr_name_120s, '120s',month).rename(columns={'new_signal':'new_signal_120s'})
-            signal_300s = merge_signal_by_pct2(expr_name_300s, '300s', month).rename(columns={'new_signal':'new_signal_300s'})
+            signal_15s = merge_signal_by_pct2(expr_name_15s, '15s', month, eval).rename(columns={'new_signal':'new_signal_15s'})
+            signal_60s = merge_signal_by_pct2(expr_name_60s, '60s', month, eval).rename(columns={'new_signal':'new_signal_60s'})
+            signal_120s = merge_signal_by_pct2(expr_name_120s, '120s',month, eval).rename(columns={'new_signal':'new_signal_120s'})
+            signal_300s = merge_signal_by_pct2(expr_name_300s, '300s', month, eval).rename(columns={'new_signal':'new_signal_300s'})
 
             merge_keys = ['ticker', 'date', 'time']
             merge_signal = signal_15s.merge(signal_60s, on=merge_keys).merge(signal_120s, on=merge_keys).merge(signal_300s, on=merge_keys)
@@ -316,6 +369,141 @@ def ensemble_batch3_pct(months, del_month=True, trading_hours=None):
 
     except Exception as e:
         print(e)
+
+
+def ensemble_batch3_pct_am(months, pool_name, del_month=True, trading_hours=None, signal_930_940='mix', eval=False):
+    trading_hours = {
+        "start_time": 93000,
+        "end_time": 113000,
+    }
+    try:
+        # table name
+        suffix = 'batch3_pct2_mix_am_pm'
+        #pool_name = 'zz500'
+        price_level = 'highprice'
+        model_type = 'lgbm'
+        database = 'strategy'
+        table_name = f"ensemble_signal_{pool_name}_{price_level}_{model_type}_{suffix}"
+        ensure_table_name(database, table_name)
+
+        for month in months:
+            # read signal and train proba
+            # expr_name_15s = f'ddb_null_factor_no_reverse_am_with_930_940_{pool_name}_highprice_lgbm_15s'
+            # expr_name_60s = f'ddb_null_factor_no_reverse_am_with_930_940_{pool_name}_highprice_lgbm_60s'
+            # expr_name_120s = f'ddb_null_factor_no_reverse_am_with_930_940_{pool_name}_highprice_lgbm_120s'
+            # expr_name_300s = f'ddb_null_factor_no_reverse_am_with_930_940_{pool_name}_highprice_lgbm_300s'
+            # expr_name_mix_60s = 'mix2_1_1_pct20_hs300_highprice_lgbm_60s'
+            # expr_name_mix_120s = 'mix3_1_1_1_pct20_hs300_highprice_lgbm_120s'
+            # expr_name_mix_300s = 'mix4_1_1_1_1_pct20_hs300_highprice_lgbm_300s'
+            expr_name_15s = f'am_model_{pool_name}_highprice_lgbm/am_model_{pool_name}_highprice_lgbm_15s'
+            expr_name_60s = f'am_model_{pool_name}_highprice_lgbm/am_model_{pool_name}_highprice_lgbm_60s'
+            expr_name_120s = f'am_model_{pool_name}_highprice_lgbm/am_model_{pool_name}_highprice_lgbm_120s'
+            expr_name_300s = f'am_model_{pool_name}_highprice_lgbm/am_model_{pool_name}_highprice_lgbm_300s'
+
+            # expr_name_15s = f'prod_202512_am_{pool_name}_highprice_lgbm/prod_202512_am_{pool_name}_highprice_lgbm_15s'
+            # expr_name_60s = f'prod_202512_am_{pool_name}_highprice_lgbm/prod_202512_am_{pool_name}_highprice_lgbm_60s'
+            # expr_name_120s = f'prod_202512_am_{pool_name}_highprice_lgbm/prod_202512_am_{pool_name}_highprice_lgbm_120s'
+            # expr_name_300s = f'prod_202512_am_{pool_name}_highprice_lgbm/prod_202512_am_{pool_name}_highprice_lgbm_300s'
+
+            if signal_930_940 == 'mix':
+                signal_15s = merge_signal_by_pct2(expr_name_15s, '15s', month, eval).rename(columns={'new_signal':'new_signal_15s'})
+            elif signal_930_940 == '15s':
+                signal_15s = merge_signal_by_pct2_930_940_15s_signal(expr_name_15s, '15s', month).rename(columns={'new_signal':'new_signal_15s'})
+            signal_60s = merge_signal_by_pct2(expr_name_60s, '60s', month, eval).rename(columns={'new_signal':'new_signal_60s'})
+            signal_120s = merge_signal_by_pct2(expr_name_120s, '120s',month, eval).rename(columns={'new_signal':'new_signal_120s'})
+            signal_300s = merge_signal_by_pct2(expr_name_300s, '300s', month, eval).rename(columns={'new_signal':'new_signal_300s'})
+
+            merge_keys = ['ticker', 'date', 'time']
+            merge_signal = signal_15s.merge(signal_60s, on=merge_keys).merge(signal_120s, on=merge_keys).merge(signal_300s, on=merge_keys)
+            merge_signal['merge_signal'] = merge_signal['new_signal_15s'] * 2 + merge_signal['new_signal_60s']*2 + merge_signal['new_signal_120s']*2 + merge_signal['new_signal_300s']*2
+            if signal_930_940 == '15s':
+                merge_signal.loc[merge_signal['time']<=94000000,'merge_signal'] = merge_signal.loc[merge_signal['time']<=94000000,'new_signal_15s']
+            if isinstance(trading_hours, dict):
+                if trading_hours['start_time'] <= trading_hours['end_time']:
+                    print(f"Trading hours:  [{trading_hours['start_time']}, {trading_hours['end_time']}]")
+                start_time = trading_hours['start_time'] * 1000
+                end_time = trading_hours['end_time'] *1000
+                merge_signal = merge_signal.query("time>= @start_time and time <= @end_time")
+            print(f"merge data size is {len(merge_signal)}")
+
+            # filter by upload month
+            merge_signal = merge_signal[(merge_signal.date < (month + 1) * 100) & (merge_signal.date > month * 100)]
+            print(f"Uploading month: {month}")
+            push_single_signal_sql(merge_signal, upload_month=[month], table_name=table_name, database=database, del_month=del_month)
+
+    except Exception as e:
+        print(e)
+
+
+def ensemble_batch3_pct_pm(months, pool_name, del_month=False, trading_hours=None, eval=False):
+    trading_hours = {
+        "start_time": 130000,
+        "end_time": 145700,
+    }
+    try:
+        # table name
+        suffix = 'batch3_pct2_mix_am_pm'
+        #pool_name = 'zz500'
+        price_level = 'highprice'
+        model_type = 'lgbm'
+        database = 'strategy'
+        table_name = f"ensemble_signal_{pool_name}_{price_level}_{model_type}_{suffix}"
+        ensure_table_name(database, table_name)
+
+        for month in months:
+            # read signal and train proba
+            # expr_name_15s = f'ddb_null_factor_no_reverse_with_930_940_{pool_name}_highprice_lgbm_15s'
+            # expr_name_60s = f'ddb_null_factor_no_reverse_with_930_940_{pool_name}_highprice_lgbm_60s'
+            # expr_name_120s = f'ddb_null_factor_no_reverse_with_930_940_{pool_name}_highprice_lgbm_120s'
+            # expr_name_300s = f'ddb_null_factor_no_reverse_with_930_940_{pool_name}_highprice_lgbm_300s'
+            # expr_name_mix_60s = 'mix2_1_1_pct20_hs300_highprice_lgbm_60s'
+            # expr_name_mix_120s = 'mix3_1_1_1_pct20_hs300_highprice_lgbm_120s'
+            # expr_name_mix_300s = 'mix4_1_1_1_1_pct20_hs300_highprice_lgbm_300s'
+
+            expr_name_15s = f'pm_model_{pool_name}_highprice_lgbm/pm_model_{pool_name}_highprice_lgbm_15s'
+            expr_name_60s = f'pm_model_{pool_name}_highprice_lgbm/pm_model_{pool_name}_highprice_lgbm_60s'
+            expr_name_120s = f'pm_model_{pool_name}_highprice_lgbm/pm_model_{pool_name}_highprice_lgbm_120s'
+            expr_name_300s = f'pm_model_{pool_name}_highprice_lgbm/pm_model_{pool_name}_highprice_lgbm_300s'
+
+            # expr_name_15s = f'prod_202512_pm_{pool_name}_highprice_lgbm/prod_202512_pm_{pool_name}_highprice_lgbm_15s'
+            # expr_name_60s = f'prod_202512_pm_{pool_name}_highprice_lgbm/prod_202512_pm_{pool_name}_highprice_lgbm_60s'
+            # expr_name_120s = f'prod_202512_pm_{pool_name}_highprice_lgbm/prod_202512_pm_{pool_name}_highprice_lgbm_120s'
+            # expr_name_300s = f'prod_202512_pm_{pool_name}_highprice_lgbm/prod_202512_pm_{pool_name}_highprice_lgbm_300s'
+
+            signal_15s = merge_signal_by_pct2(expr_name_15s, '15s', month, eval).rename(columns={'new_signal':'new_signal_15s'})
+            signal_60s = merge_signal_by_pct2(expr_name_60s, '60s', month, eval).rename(columns={'new_signal':'new_signal_60s'})
+            signal_120s = merge_signal_by_pct2(expr_name_120s, '120s',month, eval).rename(columns={'new_signal':'new_signal_120s'})
+            signal_300s = merge_signal_by_pct2(expr_name_300s, '300s', month, eval).rename(columns={'new_signal':'new_signal_300s'})
+
+            merge_keys = ['ticker', 'date', 'time']
+            merge_signal = signal_15s.merge(signal_60s, on=merge_keys).merge(signal_120s, on=merge_keys).merge(signal_300s, on=merge_keys)
+            merge_signal['merge_signal'] = merge_signal['new_signal_15s'] * 2 + merge_signal['new_signal_60s']*2 + merge_signal['new_signal_120s']*2 + merge_signal['new_signal_300s']*2
+
+            if isinstance(trading_hours, dict):
+                if trading_hours['start_time'] <= trading_hours['end_time']:
+                    print(f"Trading hours:  [{trading_hours['start_time']}, {trading_hours['end_time']}]")
+                start_time = trading_hours['start_time'] * 1000
+                end_time = trading_hours['end_time'] *1000
+                merge_signal = merge_signal.query("time>= @start_time and time <= @end_time")
+            print(f"merge data size is {len(merge_signal)}")
+
+            # filter by upload month
+            merge_signal = merge_signal[(merge_signal.date < (month + 1) * 100) & (merge_signal.date > month * 100)]
+            print(f"Uploading month: {month}")
+            push_single_signal_sql(merge_signal, upload_month=[month], table_name=table_name, database=database, del_month=del_month)
+
+    except Exception as e:
+        print(e)
+
+
+def ensemble_batch3_pct_mix_am_pm(months, eval=False):
+    ensemble_batch3_pct_am(months, pool_name='zz2000', del_month=True, trading_hours=None, eval=eval)
+    ensemble_batch3_pct_pm(months, pool_name='zz2000', del_month=False, trading_hours=None, eval=eval)
+
+
+def ensemble_batch3_pct_mix_am_pm_930_940_15s_signal(months):
+    ensemble_batch3_pct_am(months, pool_name='zz1000', del_month=True, trading_hours=None, signal_930_940='15s')
+    ensemble_batch3_pct_pm(months, pool_name='zz1000', del_month=False, trading_hours=None)
 
 
 def ensemble_bond_etf_6m(months, trading_hours=None):
@@ -375,13 +563,17 @@ def check_dist(signal_data, column,  time_bin=[940, 1000, 1030, 1130, 1400, 1500
 
     return  abs_signal
 
+
 if __name__ == "__main__":
-    months = [202501, 202502,202503,202504, 202505,202506, 202507]
+    months = [202512]
+    eval=False
     # ensemble_bond_etf_6m(months)
     # signal = read_signal_and_adjust_main_signal_enhance('ddb_null_factor_no_re_all_new_6m_hs300_highprice_lgbm_300s', '300s', 202506)
     # ensemble_all_new_6m_enhance(months)
     #ensemble_all_new_6m(months)
-    ensemble_batch3_pct(months, del_month=False) # zz500
+    # ensemble_batch3_pct(months, del_month=True)
+    ensemble_batch3_pct_mix_am_pm(months, eval=False)
+    # ensemble_batch3_pct_mix_am_pm_930_940_15s_signal(months)
 
 
 
